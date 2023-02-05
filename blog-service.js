029a0f86c531329c5b2b21e633ffeb9abf03606e
nrrri <nrrri.cho@gmail.com>
2023-02-05 12:28:13 -0500

    update

--- blog-service.js
+++ blog-service.js
@@ -1,3 +1,61 @@
-module.exports.readMessage = () => {
-    console.log("reading blog from file " + __filename)
+const fs = require("fs")
+let categories = []
+let posts = []
+
+
+module.exports.initialize = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+            if (err) {
+                reject()
+            } else {
+                console.log(data)
+                posts = JSON.parse(data)
+
+                fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+                    if (err) {
+                        reject()
+                    } else {
+                        console.log(data)
+                        categories = JSON.parse(data)
+                        resolve()
+                    }
+                })
+            }
+        })
+    })
+}
+
+
+// export Categories array
+module.exports.getCategories = () => {
+    return new Promise((resolve, reject) => {
+        if (categories.length > 0) {
+            resolve(categories)
+        } else {
+            reject("No results returned")
+        }
+    })
+}
+
+// export Posts array
+module.exports.getAllPosts = () => {
+    return new Promise((resolve, reject) => {
+        if (posts.length > 0) {
+            resolve(posts)
+        } else {
+            reject("No results returned")
+        }
+    })
+}
+
+// export Blog pub;ish == true
+module.exports.getPublishedPosts = () => {
+    return new Promise((resolve, reject) => {
+        if (posts.length > 0 && posts.indexOf("published") == true) {
+            resolve(posts)
+        } else {
+            reject("No results returned")
+        }
+    })
 }
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: __Narisorn Chowarun__ Student ID: __169007218__ Date: __Fubruary 4, 2023_
+*
+*  Cyclic Web App URL: ________________________________________________________
+*
+*  GitHub Repository URL: ___https://github.com/nrrri/web322-app.git__
+*
+********************************************************************************/ 
+
+
+
 var express = require("express");
 var app = express();
 var path = require("path");
-var blog = require("blog-service.js")
+var blog = require("./blog-service.js")
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -26,6 +41,46 @@ app.get("/about", function(req,res){
     
 });
 
+// setup another route to listen on /blog
+app.get("/blog",(req,res) => {
+blog.getPublishedPosts().then((posts) => {
+    res.json(posts)
+    //res.send("TODO:get all pasts who have published==true");
+}).catch((err) => {
+    console.log(err)
+    res.send(err)
+})
+})
+
+// setup another route to listen on /posts
+app.get("/posts",(req,res) => {
+    blog.getAllPosts().then((posts) => {
+        res.json(posts)
+    }).catch((err)=> {
+        console.log(err)
+        res.send(err)
+    })
+    //res.send("this is in posts new");
+    })
+
+// setup another route to listen on /categories
+app.get("/categories",(req,res) => {
+    blog.getCategories().then((categories) => {
+        res.json(categories)
+    }).catch((err) => {
+        console.log(err)
+        res.send(err)
+    })
+    //res.send("this is in categories new");
+    })
+
+
+// setup another route to listen on err
+app.use((req, res) => {
+    res.status(404).send("Page Not Found")
+  })
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+blog.initialize().then(() => {
+    app.listen(HTTP_PORT, onHttpStart);
+})
