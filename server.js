a8c03950ac881177a71e968f74bfaf08e0776f13
nrrri <nrrri.cho@gmail.com>
2023-03-07 01:01:58 -0500

    cyclic link

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 * 
 *  Name: __Narisorn Chowarun__ Student ID: __169007218__ Date: __Fubruary 4, 2023_
 *
-*  Cyclic Web App URL: ___https://viridian-armadillo-garb.cyclic.app/__
+*  Cyclic Web App URL: ___https://plain-ruby-rabbit.cyclic.app__
 *
 *  GitHub Repository URL: ___https://github.com/nrrri/web322-app.git__
 *
